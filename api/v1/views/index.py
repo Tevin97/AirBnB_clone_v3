701e902000e0bc9c53bc5bb0c629a4f1d008a960
Tevin Murimi Mwenda <tevinmurimimwenda@gmail.com>
2023-10-31 18:29:52 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""index of views"""
+"""
+Create a route `/status` on the object app_views
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
